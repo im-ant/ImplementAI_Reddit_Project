a3740d8ac8863dff5b9822f67dded43b0b2a0864
Jing <liujingyun0209@gmail.com>
2017-09-30 18:52:54 -0400

    use ticks instead of company names

--- daily_post.py
+++ daily_post.py
@@ -3,10 +3,22 @@
 import json
 import time
 import datetime
+import sys
+import re
 import pandas as pd
 from pandas import DataFrame as df
 
-def reformat(file_in):
+
+def tick2name():
+    df = pd.read_csv('/data/ImplementReddit/companies/constituents.csv')
+    symb_name = {}
+    for line in df[['Symbol', 'Name']].values:
+        name = line[1].lower()
+        name_ = re.sub(r'(\scorp.{2}|\sinc.*|\scos.*|\&\sco.*|\sco.*|ltd.|the\s)',"",name) # strip off the Inc/Corp stuff
+        symb_name[name_] = line[0]
+    return symb_name
+
+def reformat(file_in, name_symb):
     """
     reads the json file of file_in, extract [Title | Subreddits | Total votes | Upvotes | Downvotes] for each day for each company. 
     return: {company_name: {date1: extraction, date2:extraction}, ...}
@@ -15,10 +27,11 @@ def reformat(file_in):
     comp_post_daily = {} # {company_name: {date1:[post],date2:[post]},...}
     for comp,posts in data.items():
         # print("Company name %s, number of posts: %s"%(comp, len(posts)))
+        comp = name_symb[comp]
         for post in posts:
             t = post["created_utc"]
             d = datetime.datetime.utcfromtimestamp(float(t)).strftime('%Y-%m-%d,%H:%M:%SZ') # formatted date-time
-            day_ = int(d.split(",")[0].split("-")[2])
+            day_ = d.split(",")[0]
 
             ## extract from the post: Title | Subreddits | Total votes |  Downvotes | Upvotes
             info = [post["title"], post["subreddit"],-post["downs"], post["score"] + post["downs"]]
@@ -59,7 +72,8 @@ if __name__=="__main__":
     # fin = "datas/post.json"
     fin = sys.argv[1]
     fout = sys.argv[2]
-    X_feature = reformat(fin)
+    name_symb = tick2name()
+    X_feature = reformat(fin, name_symb)
     out = to_csv(X_feature,fout)
    
 
