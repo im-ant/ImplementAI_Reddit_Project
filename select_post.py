03aa831ceeeb683f3074f5db869c66b8f0c0dfaf
Jing <liujingyun0209@gmail.com>
2017-09-30 16:24:10 -0400

    fix memory issue

--- select_post.py
+++ select_post.py
@@ -8,7 +8,7 @@ import sys
 fp = sys.argv[1] # input file path
 fout = sys.argv[2] #output file path
 
-df = pd.read_csv('/data/ImplementReddit/companies/constituents.csv')
+df = pd.read_csv('datas/constituents.csv')
 symb_name = {}
 for line in df[['Symbol', 'Name']].values:
     name = line[1].lower()
@@ -18,24 +18,22 @@ symbs = set(symb_name.keys())
 names =  set(symb_name.values())
 print("Total %s companies to consider"%(len(symbs)))
 
+count = 0
+data_company = {} # {company_name: [post1, post2...]}
 data = []
 with open(fp) as f:
     for line in f:
-        data.append(json.loads(line))
-
-count = 0
-data_company = {} # {company_name: [post1, post2...]}
-for d in data:
-    title = d["title"].lower()
-    if set(title.split()).intersection(names):
-        comps = set(title.split()).intersection(names)
-        # print(comps)
-        count += 1
-        for comp in comps:
-            if not data_company.get(comp):
-                data_company[comp] = [d]
-            else:
-                data_company[comp].append(d)
+        d = json.loads(line) 
+        title = d["title"].lower()
+        if set(title.split()).intersection(names):
+            comps = set(title.split()).intersection(names)
+            # print(comps)
+            count += 1
+            for comp in comps:
+                if not data_company.get(comp):
+                    data_company[comp] = [d]
+                else:
+                    data_company[comp].append(d)
 print(" Selected %s posts from %s"%(count, fp))
 
 json.dump(data_company, open(fout,"w"))
